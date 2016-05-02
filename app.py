1daac880d1dfc1f8913d8c1cf52a292aebe2cd74
kennburnett <kennburnett@users.noreply.github.com>
2016-05-02 16:38:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ import os
 @route('/')
 def index():
 	return "<h1> hello OpenShift Ninja without DB</h1>"
-
+"Heres is some text for changes"
 if __name__ == '__main__':
 	run(host='0.0.0.0', port=8080, debug=True)
